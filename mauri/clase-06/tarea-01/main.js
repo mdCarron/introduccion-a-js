e7bbcc75ff44537d03fd39ff8c48aebbfbc75567
mdCarron <59117899+mdCarron@users.noreply.github.com>
2020-05-12 15:24:58 -0300

    actualiza tarea 1

--- mauri/clase-06/tarea-01/main.js
+++ mauri/clase-06/tarea-01/main.js
@@ -1,24 +1,5 @@
-console.log('Hola Mundo!')
-
-/*
-TAREA: Empezar preguntando cuánta gente hay en el grupo familiar.
-Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
-Al hacer click en "calcular", mostrar en un  pre-elementoexistente la mayor edad, la menor edad y el promedio del grupo familiar.
-
-Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
-*/
-
-
-/*
-TAREA:
-Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
-Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
-
-Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
-*********************************************************************************/
 $botonSiguiente = document.querySelector('#boton-siguiente');
 $botonSiguiente.onclick = function (){
-    //borrarIntegrantes()
     
     const $cantidadIntegrantes = document.querySelector('#cantidad-integrantes');
     const cantidadIntegrantes = Number($cantidadIntegrantes.value);
@@ -32,6 +13,7 @@ function crearIntegrantes(cantidadIntegrantes){
         crearIntegrante(i);
     }
 }
+
 function crearIntegrante(indice){
     const $div = document.createElement('div');
     $div.className = 'integrante';
---
+++ mauri/clase-06/tarea-01/tests.js
@@ -0,0 +1,68 @@
+/* pruebaValidarNombre();
+pruebaValidarCiudad();
+pruebaValidarComportamiento();
+pruebaValidarRegalo();
+
+function pruebaValidarNombre(){
+    console.assert(
+        validarNombre('11111111') === 'El campo nombre solo acepta letras.',
+        'validarNombre no valida que el usuario ingreso un nombre solo con letras.'
+    );
+
+    console.assert(
+        validarNombre('') === 'Este campo debe tener al menos 1 caracter.', 
+        'validarNombre no valida con un string vacio.'
+    );
+
+    console.assert(
+        validarNombre('111111111111111111111111111111111111111111111111111') === 'Este campo debe tener menos de 50 caracteres.',
+        'validarNombre no valida que el campo tenga menos de 50 caracteres.'
+    );
+
+}
+
+function pruebaValidarCiudad(){
+    console.assert(
+        validarCiudad('Buenos Aires') === '',
+        'validarCiudad no valida que se seleciono una opcion valida de ciudad.'
+    );
+    console.assert(
+        validarCiudad('') === 'Por favor seleccionar una ciudad.', 
+        'validarCiudad no valida que hay una ciudad seleccionada..'
+    );
+}
+
+function pruebaValidarComportamiento(){
+    console.assert(
+        validarComportamiento('muy_bueno') === '',
+        'validarComportamiento no valida que se seleciono una opcion valida.'
+    );
+    console.assert(
+        validarComportamiento('') === 'Por favor seleccionar una opcion.',
+        'validarComportamiento no valida que haya una opcion selecionada.'
+    )
+}
+
+function pruebaValidarRegalo(){
+    console.assert(
+        validarRegalo('Regalo') === '',
+        'validarRegalo no valida que se ingreso un texto valido en el campo.'
+    );
+    console.assert(
+        validarRegalo('') === 'Por favor dejar un mensaje para papa noel.',
+        'validarRegalo no valida que en el campo de texto esta vacio.'
+    )
+    console.assert(
+        validarRegalo('111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') === 'Estas pidiendo mucho a papa noel',
+        'validarRegalo no valida que supera el maximo de 100 caracteres.'
+    )
+    console.assert(
+        validarRegalo('...,.,.,') === 'El campo de la descripcion solo acepta letras y numeros.',
+        'validarRegalo no valida que no se ingresaron solo letras y numeros en el capo de texto.'
+
+    );
+}
+//al validar las opciones tienen que ser las mismas que las escritas en la funcion, porque sino no funciona. Ademas siempre aclarar bien cual es el mensaje de error para identificar mas facil de donde viene el problema.
+ */
+
+//hay que validar que las funciones que estan haciendo aperaciones matematicas den bien.
