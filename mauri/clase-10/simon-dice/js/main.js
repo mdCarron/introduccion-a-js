f8f2cae1939a39bdd552be813285a383314a97e1
mdCarron <59117899+mdCarron@users.noreply.github.com>
2020-06-10 00:06:54 -0300

    termina logica de simon-dice

--- mauri/clase-10/simon-dice/js/main.js
+++ mauri/clase-10/simon-dice/js/main.js
@@ -1,19 +1,124 @@
-console.log('Hola Mundo!');
+//GLOBALES
+let secuenciaUsuario = [];
+let secuenciaMaquina = [];
+let ronda = 0;
 
-/* 
-LOGICA DEL JUEGO EN ALTO NIVEL
-a nivel global se define dos arrays,uno es toda la secuencia que ejecuta la maquina, y el otro es toda la secuencia que ejecuta el usuario. y ademas se declara la ronda.
 
-selector del boton jugar, que inicia la funcion comenzarJuego   ****se puede selccionar una etiqueta con tributo de la misma forma que se hace en css (button[type=button]).
+//JUEGO
+document.querySelector('#jugar').onclick = comenzarJuego;
 
-functin comenzarJuego
+actualizarEstado('Apretá Jugar! para comenzar');
+actualizarNumeroRonda('~');
+bloquearInputUsuario();
+
+function comenzarJuego() {
     reiniciarEstado();
     manejarRonda();
-
-function manejarRonda {
-    actualizar estado('Turno de la maquina') //funcion para ir actauliazndo el alert.
-    bloquearInputUsuario() //funcion para que el usuario no pueda intervenir mientras juega la computadora.
-
-    
 }
-*/
+
+
+function manejarRonda() {
+    actualizarEstado('Turno de la maquina...');
+    bloquearInputUsuario();
+
+    const $nuevoCuadro = obtenerCuadroAleatorio();
+    secuenciaMaquina.push($nuevoCuadro);
+
+    secuenciaMaquina.forEach(
+        function($cuadro, index) {
+            const RETRASO_MS = (index + 1) * 1000;
+            setTimeout(
+                function(){
+                    resaltar($cuadro);
+                }, RETRASO_MS
+            )
+        }
+    )
+
+    const RETRASO_TURNO_JUGADOR = (secuenciaMaquina.length + 1) * 1000;
+    setTimeout(function(){
+        actualizarEstado('Turno del jugador...');
+        desbloquearInputUsuario();
+    }, RETRASO_TURNO_JUGADOR);
+
+    secuenciaUsuario = [];
+    ronda++;
+    actualizarNumeroRonda(ronda);
+}
+
+function manejarInputUsuario(e) {
+    const $cuadro = e.target;
+    resaltar($cuadro);
+    secuenciaUsuario.push($cuadro);
+
+    const $cuadroMaquina = secuenciaMaquina[secuenciaUsuario.length - 1];
+    if ($cuadro.id !== $cuadroMaquina.id){
+        perder();
+        return
+    }
+
+    if (secuenciaUsuario.length === secuenciaMaquina.length){
+        bloquearInputUsuario();
+        setTimeout(manejarRonda, 1000);
+    }
+}
+
+
+//FUNCIONES
+function reiniciarEstado() {
+    secuenciaUsuario = [];
+    let secuenciaMaquina = [];
+    let ronda = 0;
+}
+
+
+function actualizarEstado (estado, error = false){
+    const $estado = document.querySelector('#estado');
+    $estado.innerText = estado;
+
+    if(error){
+        $estado.classList.remove('alert-primary');
+        $estado.classList.add('alert-danger');
+    }else{
+        $estado.classList.remove('alert-danger');
+        $estado.classList.add('alert-primary');
+    }
+}
+
+function actualizarNumeroRonda(ronda) {
+    document.querySelector('#ronda').innerText = ronda;
+}
+
+function obtenerCuadroAleatorio() {
+    const $cuadros = document.querySelectorAll('.cuadro');
+    const $indice = Math.floor(Math.random() * $cuadros.length);
+
+    return $cuadros[$indice]
+}
+
+function resaltar($cuadro) {
+    $cuadro.style.opacity = 1;
+    setTimeout(function(){
+        $cuadro.style.opacity = 0.5;
+    }, 500);
+}
+
+function bloquearInputUsuario(){
+    document.querySelectorAll('.cuadro').forEach(
+        function($cuadro){
+            $cuadro.onclick = function(){
+
+            }
+        })
+}
+
+function desbloquearInputUsuario() {
+    document.querySelectorAll('.cuadro').forEach(function($cuadro){
+        $cuadro.onclick = manejarInputUsuario;
+    })
+}
+
+function perder(){
+    bloquearInputUsuario();
+    actualizarEstado(`Perdiste! Duraste ${ronda} ronda/s. Podes comenzar de nuevo aprentando en Jugar!`, true);
+}
